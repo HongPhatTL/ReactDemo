cae608f99b991d42072b4dd8264a4e8df4bd2250
phat.pham <phat.pham@terralogic.com>
2022-11-02 16:13:04 +0700

    16:12

--- src/components/hello/hello.jsx
+++ src/components/hello/hello.jsx
@@ -3,7 +3,7 @@ import './hello.css'
 const Hello = () => {
     return(
         <div>
-        <h1>Hello World</h1>
+        <h1>Dennis GAYYYYYY</h1>
     </div>
     )
 }
--- src/components/images/image.jsx
+++ src/components/images/image.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Dennis from '/home/phatpham/Documents/Codes/Javascript/phat_app/src/assets/images/dennis.jpg'
 import './image.css'
 const Image = () => {
     return(
         <div>
-        {/* <img src={Zero2} style={{ width: '600px',height:'600px'}}/> */}
-        <img src="https://reactjs.org/logo-og.png" alt="react logo" style={{ width: '600px', height:'400px' }} href="www.google.com" class="center"/>
+        <img src={Dennis} style={{ width: '600px',height:'600px'}}/>
+        {/* <img src="https://reactjs.org/logo-og.png" alt="react logo" style={{ width: '600px', height:'400px' }} href="www.google.com" class="center"/>? */}
     </div>
     )
 }
